3c408df39fa73dca1566dc6bf7dbdf83bd12211e
pex-k <peksi@lazyanimator.com>
2026-03-17 14:52:19 +0200

    Added zls to lsp

--- init.lua
+++ init.lua
@@ -616,6 +616,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
+        zls = {},
 
         stylua = {}, -- Used to format Lua code
 
